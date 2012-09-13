2f69be6eea9f248167ef1ae1f10a1b8e12ef75c0
lars berg <laserstorm@gmail.com>
2012-09-12 21:46:49 -0700

    added README

---
+++ README.md
@@ -0,0 +1,9 @@
+#ofxScene
+
+ofxScene is a simple scene graph used to used to quickly create and render geometry
+
+
+
+
+
+
--- src/includes/Material.cpp
+++ src/includes/Material.cpp
@@ -1,9 +1,5 @@
 //
 //  Material.cpp
-//  ofxSceneExample
-//
-//  Created by lars berg on 8/18/12.
-//
 //
 
 #include "Material.h"
